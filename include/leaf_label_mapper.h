f62d0b0b8edebe57709aa885a31557dc2f43c1d7
p3732 <p3732@users.noreply.github.com>
2017-07-01 16:39:49 +0200

    leaf_label_mapper - made methods static

--- include/leaf_label_mapper.h
+++ include/leaf_label_mapper.h
@@ -10,13 +10,13 @@ public:
     static std::map<const char*, size_t> label_to_leaf_id;
     static char **species_names;
 
-    char* get_label_from_leaf_id(const size_t leaf) {
+    static char* get_label_from_leaf_id(const size_t leaf) {
         assert(species_names != nullptr);
         
         return species_names[leaf];
     };
 
-    size_t get_leaf_id_from_label(const char* label) {
+    static size_t get_leaf_id_from_label(const char* label) {
         assert(label_to_leaf_id.count(label) == 1);
         
         return label_to_leaf_id[label];
